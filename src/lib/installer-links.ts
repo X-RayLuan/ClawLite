9b10ef5f1a418c07aab3888914bd13f0f9a234f4
m1 <m1@m1deMac-mini.local>
2026-04-10 14:04:51 +0800

    fix: point windows installer download to v1.3.147

--- src/lib/installer-links.ts
+++ src/lib/installer-links.ts
@@ -1,6 +1,6 @@
 export const INSTALLER_VERSION = {
   macos: 'v1.3.137',
-  windows: 'v1.3.146'
+  windows: 'v1.3.147'
 } as const
 
 const MAC_RELEASE_BASE = `https://github.com/X-RayLuan/ClawLite-Brand-Installer/releases/download/${INSTALLER_VERSION.macos}`
