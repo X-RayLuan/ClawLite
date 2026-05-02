30c137e7571b9ff0b72972ecbb1cf0a2cb9016fb
Clawlite PD <pd@clawlite.ai>
2026-05-02 18:17:20 +0800

    chore: update installer to v1.5.181

--- src/lib/installer-links.ts
+++ src/lib/installer-links.ts
@@ -1,6 +1,6 @@
 export const INSTALLER_VERSION = {
-  macos: 'v1.5.180',
-  windows: 'v1.5.180'
+  macos: 'v1.5.181',
+  windows: 'v1.5.181'
 } as const
 
 const MAC_RELEASE_BASE = `https://github.com/X-RayLuan/ClawLite-Brand-Installer/releases/download/${INSTALLER_VERSION.macos}`
