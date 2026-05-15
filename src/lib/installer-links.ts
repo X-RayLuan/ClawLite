20e2dfa171478216102da92df28ce0e06a3d10c4
Clawlite PD <pd@clawlite.ai>
2026-05-15 21:57:23 +0800

    chore: update installer to v1.5.236

--- src/lib/installer-links.ts
+++ src/lib/installer-links.ts
@@ -1,6 +1,6 @@
 export const INSTALLER_VERSION = {
-  macos: 'v1.5.234',
-  windows: 'v1.5.234'
+  macos: 'v1.5.236',
+  windows: 'v1.5.236'
 } as const
 
 const MAC_RELEASE_BASE = `https://github.com/X-RayLuan/ClawLite-Installer/releases/download/${INSTALLER_VERSION.macos}`
